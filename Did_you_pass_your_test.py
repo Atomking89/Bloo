d6e4cfd32ef6254266dd1f8772d44c0bea5e22f1
Atomking89 <Sagan.Atom.Warner@icloud.com>
2026-06-17 19:19:54 +0000

    did you pass your test??????

---
+++ Did_you_pass_your_test.py
@@ -0,0 +1,9 @@
+grade = int(input("What score did you get?"))
+if grade >= 100:
+    print("YOU GOT A S+ GREAT JOB!")
+elif grade >= 90:
+    print("YOU GOT AN A. AMAZING!")
+elif grade >= 75:
+    print("You got a B.That's okay.")
+elif grade <= 60:
+    print("You got a F and failed your test. Better luck next time.")
